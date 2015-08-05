c36aa2d4a37e06e4393cb5f1b48703cd4c367be0
Uma Maheswara <umamahesh330@gmail.com>
2015-08-05 10:34:40 +0530

    latest files

---
+++ test.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+describe("Add testcases", function () {
+    it("AddCorrect1", function () {
+        expect(2 + 3).toEqual(5);
+    });
+
+    it("AddCorrect2", function () {
+        expect(2 * 3).toEqual(7);
+    });
+
+    it("AddCorrect3", function () {
+        expect(2 * 3).toEqual(7);
+    });
+});
